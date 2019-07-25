267d51911a5bb169052c3506f032452f3fd15113
Patrick Kim <pkim050@gmail.com>
2019-07-25 13:26:28 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props =>
+  <div class="review-list">
+    <ul class="review">
+
+    </ul>
+  </div>
+
+export default MovieReviews
