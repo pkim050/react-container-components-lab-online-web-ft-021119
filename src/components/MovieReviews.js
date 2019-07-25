f07518790050101bbd14b1645f88c8f33987603a
Patrick Kim <pkim050@gmail.com>
2019-07-25 21:01:16 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,11 +1,10 @@
 // Code MovieReviews Here
 import React from 'react'
 
-const MovieReviews = props =>
-  <div className="review-list">
-    <div className="review">
-      console.log(props)
-    </div>
+const MovieReviews = ({reviews}) => {
+  return <div className="review-list">
+     {reviews.map(review => <div className="review">{review.summary_short}</div>)}
   </div>
+}
 
 export default MovieReviews
