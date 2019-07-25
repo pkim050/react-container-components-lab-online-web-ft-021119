6cb71ebab26ef6b80fd3bf038cc0cccd6745ddad
Patrick Kim <pkim050@gmail.com>
2019-07-25 13:30:02 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -8,7 +8,20 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 
 // Code SearchableMovieReviewsContainer Here
 class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
 
+    }
+  }
+
+  render() {
+    return (
+      <div class="searchable-movie-reviews">
+
+      </div>
+    )
+  }
 }
 
 export default SearchableMovieReviewsContainer
