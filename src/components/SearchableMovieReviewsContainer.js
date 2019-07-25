999a82b59c10e713da078893c20e6ceaaf99fd91
Patrick Kim <pkim050@gmail.com>
2019-07-25 13:27:26 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,8 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+}
+
+export default SearchableMovieReviewsContainer
